03321158031b57194fc51410e50d5f15af15c4ba
Dylan Ballback <Dylanballback19@gmail.com>
2023-12-10 03:21:45 -0500

    basic periodic table layout on website

--- periodic_table_app/app.py
+++ periodic_table_app/app.py
@@ -1,12 +1,16 @@
 from flask import Flask, render_template
-from database.database import get_elements # Import the get_elements function from database.py
+from database.database import * # Import the get_elements function from database.py
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    elements = get_elements()  # Use the get_elements function to get the data
-    return render_template('periodic_table.html', elements=elements)  # Pass the elements to the template
+    elements = get_elements()
+    indexed_periodic_table = [(i, row) for i, row in enumerate(periodic_table)]
+    return render_template('periodic_table.html', periodic_table=indexed_periodic_table)
 
 if __name__ == '__main__':
     app.run(debug=True)
+    elements = get_elements()
+    if elements:
+        print(elements[0].keys())  # This will print the column names of the first element
